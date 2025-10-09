ab7c9f1e80adb465f913ccd02b63a70c0d043a1a
Cfabiola25 <est_nf_cano@fesc.edu.co>
2025-10-08 23:34:30 -0500

    fix(database): remove locale settings from siget_db creation script

--- db/dumps/postgres/siget_schema.sql
+++ db/dumps/postgres/siget_schema.sql
@@ -9,8 +9,6 @@ CREATE DATABASE siget_db
   WITH 
   OWNER = postgres
   ENCODING = 'UTF8'
-  LC_COLLATE = 'es_CO.UTF-8'
-  LC_CTYPE = 'es_CO.UTF-8'
   TABLESPACE = pg_default
   CONNECTION LIMIT = -1;
 
